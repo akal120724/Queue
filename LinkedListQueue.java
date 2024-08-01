a0145eb1ee64143f52de052d7f18100156965524
akalyakumar <146169496+akal120724@users.noreply.github.com>
2024-08-01 11:58:24 +0530

    Create LinkedListQueue.java
    Output:
    
    Element 6 added to the queue
    Element 3 added to the queue
    Front of the queue:6 Rear of the queue:3
    Element 12 added to the queue
    Element 24 added to the queue
    Element 6 removed from the queue
    Element 3 removed from the queue
    Element 9 added to the queue
    Front of the queue:12 Rear of the queue:9

---
+++ LinkedListQueue.java
@@ -0,0 +1,70 @@
+class LinkedListQueue  
+{  
+ private Node front, rear;  
+ private int queueSize;
+ private class Node {  
+ int data;  
+ Node next;  
+ }  
+
+ public LinkedListQueue()  
+ {  
+ front = null;  
+ rear = null;  
+ queueSize = 0;  
+ }  
+
+ public boolean isEmpty()  
+ {  
+ return (queueSize == 0);  
+ }    
+
+ public int dequeue()  
+ {  
+ int data = front.data;  
+ front = front.next;  
+ if (isEmpty())  
+ {  
+ rear = null;  
+ }  
+ queueSize--;  
+ System.out.println("Element " + data+ " removed from the queue");  
+ return data;  
+ }  
+ 
+ public void enqueue(int data)  
+ {  
+ Node oldRear = rear;  
+ rear = new Node();  
+ rear.data = data;  
+ rear.next = null;  
+ if (isEmpty())  
+ {  
+ front = rear;  
+ }  
+ else  {  
+ oldRear.next = rear;  
+ }  
+ queueSize++;  
+ System.out.println("Element " + data+ " added to the queue");  
+ }  
+
+ public void print_frontRear() {  
+     System.out.println("Front of the queue:" + front.data  
+     + " Rear of the queue:" + rear.data);  
+ }  
+}  
+class QueueLLImplementation{  
+ public static void main(String a[]){  
+ LinkedListQueue queue = new LinkedListQueue();  
+ queue.enqueue(6);  
+ queue.enqueue(3);  
+ queue.print_frontRear();  
+ queue.enqueue(12);  
+ queue.enqueue(24);  
+ queue.dequeue();  
+ queue.dequeue();  
+ queue.enqueue(9);   
+ queue.print_frontRear();  
+ }  
+}  
